60b867edaee819f98d700a087070095017f76559
ri0t <riot@c-base.org>
2019-03-09 18:29:48 +0100

    Fixed packaging, added Trove classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 # HFOS - Hackerfleet Operating System
 # ===================================
-# Copyright (C) 2011-2018 Heiko 'riot' Weinen <riot@c-base.org> and others.
+# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -23,15 +23,31 @@ __license__ = "AGPLv3"
 
 from setuptools import setup, find_packages
 
-setup(name="hfos-webguides",
-      version="0.0.1",
-      description="hfos-webguides - import module for web based geo guides",
-      author="Hackerfleet Community",
-      author_email="riot@c-base.org",
-      url="https://github.com/hackerfleet/hfos-webguides",
-      license="GNU Affero General Public License v3",
-      packages=find_packages(),
-      long_description="""HFOS - WebGuides
+setup(
+    name="hfos-webguides",
+    version="0.0.1",
+    description="hfos-webguides - import module for web based geo guides",
+    author="Hackerfleet Community",
+    author_email="riot@c-base.org",
+    url="https://github.com/hackerfleet/hfos-webguides",
+    license="GNU Affero General Public License v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Isomer :: 1',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages(),
+    long_description="""HFOS - WebGuides
 ================
 
 A module to import web guide data. 
@@ -40,12 +56,12 @@ Supported guides:
 
 This software package is a plugin module for HFOS.
 """,
-      dependency_links=[],
-      install_requires=[
-          'isomer>=1.0.0'
-      ],
-      entry_points="""[isomer.components]
-    guides=hfos.guides.guide_manager:GuideManager
+    dependency_links=[],
+    install_requires=[
+        'isomer>=1.0.0'
+    ],
+    entry_points="""[isomer.components]
+    guides=isomer.guides.guide_manager:GuideManager
     """,
-      test_suite="tests.main.main",
-      )
+    test_suite="tests.main.main",
+)
